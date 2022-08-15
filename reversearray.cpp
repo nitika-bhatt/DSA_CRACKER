e86493cfeaf29d7687857ef30c567e7e2120d8d1
Nitika <78733038+nitika-bhatt@users.noreply.github.com>
2022-08-15 19:45:02 +0530

    Update reversearray.cpp

--- reversearray.cpp
+++ reversearray.cpp
@@ -20,3 +20,41 @@ int main() {
     }
 return 0;
 }
+
+//my code was this but it failed the testcases
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+  
+//     int T;
+//     cin>>T;
+//     while(T-->0)
+//     {
+//         int N;
+//         cin>>N;
+//         if(N>=1 && N<=100)
+//         {
+//         int A[N];
+//         int temp=0;
+        
+//         for(int i=0;i<N;i++)
+//         {
+//             cin>>A[i];
+//         }
+//         for(int i=0;i<N/2;i++)
+//         {
+//             temp=A[i];
+//             A[i]=A[(N-i)-1];
+//             A[(N-i)-1]=temp;
+//         }
+//         for(int i=0;i<N;i++)
+//         {
+//             cout<<A[i]<<" ";
+            
+//         }}
+       
+// }
+//     return 0;
+// }
